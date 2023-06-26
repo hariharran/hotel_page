f77ffe590fad0672883f553d4a5466fa959632d1
hariharran <121088046+hariharran@users.noreply.github.com>
2023-06-26 16:15:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function scrollhome() {
+    window.scrollTo(0,0);
+  }
+  function scrollabout() {
+    window.scrollTo(0, 2120);
+  }
+  function scrollcontact() {
+    window.scrollTo(0, 2400);
+  }
